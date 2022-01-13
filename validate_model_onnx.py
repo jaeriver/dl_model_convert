f4808b6aadd747bedc4af3b03b3a2c267045261d
최재강 <chl8273@naver.com>
2022-01-13 17:27:20 +0900

    Update validate_model_onnx.py

--- validate_model_onnx.py
+++ validate_model_onnx.py
@@ -7,7 +7,15 @@ parser.add_argument('--model',default='mobilenet-13.onnx' , type=str)
 args = parser.parse_args()
 onnx_model = args.model
 
-img = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=np.float32)
+def make_dataset(batch_size,size):
+    image_shape = (size, size,3)
+    data_shape = (batch_size,) + image_shape
+
+    data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+
+    return data,image_shape
+
+data, image_shape = make_dataset(1, 224)
   
 session = ort.InferenceSession(onnx_model)
 
@@ -16,6 +24,6 @@ inname = [input.name for input in session.get_inputs()]
 outname = [output.name for output in session.get_outputs()]
 print(inname, outname)
 
-res = session.run(outname, {inname[0]: img})
+res = session.run(outname, {inname[0]: data})
 print("the expected result is \"7\"")
 print("the digit is classified as \"%s\" in ONNXRruntime"%np.argmax(res))
