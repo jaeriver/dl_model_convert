571045d1a1b7b1ea9b3e76be5b25424d2b1c39e7
최재강 <chl8273@naver.com>
2022-01-13 17:31:46 +0900

    Update validate_model_onnx.py

--- validate_model_onnx.py
+++ validate_model_onnx.py
@@ -1,10 +1,13 @@
 import numpy as np
 import onnxruntime as ort
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model',default='mobilenet-13.onnx' , type=str)
+parser.add_argument('--batch_size',default=1 , type=int)
 args = parser.parse_args()
+batch_size = args.batch_size
 onnx_model = args.model
 
 def make_dataset(batch_size,size):
@@ -15,7 +18,7 @@ def make_dataset(batch_size,size):
 
     return data,image_shape
 
-data, image_shape = make_dataset(2, 224)
+data, image_shape = make_dataset(batch_size, 224)
   
 session = ort.InferenceSession(onnx_model)
 
@@ -24,6 +27,8 @@ inname = [input.name for input in session.get_inputs()]
 outname = [output.name for output in session.get_outputs()]
 print(inname, outname)
 
+start_time = time.time()
 res = session.run(outname, {inname[0]: data})
+print('batch_size: ', batch_size,'time: ', time.time() - start_time)
 print(res)
 print("the digit is classified as \"%s\" in ONNXRruntime"%np.argmax(res))
