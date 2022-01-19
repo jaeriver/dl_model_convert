764206c2dddf8ea7245a3aee0dfdfb333660fdb3
최재강 <chl8273@naver.com>
2022-01-19 21:49:44 +0900

    change  mobilenet v2

--- save_tf_model.py
+++ save_tf_model.py
@@ -4,30 +4,30 @@ from tensorflow.keras.applications import (
 #     vgg16,
 #     vgg19,
 #     resnet,
-    resnet50,
+#     resnet50,
 #     resnet_v2,
-    inception_v3,
+#     inception_v3,
 #     inception_resnet_v2,
 #     mobilenet,
 #     densenet,
 #     nasnet,
-#     mobilenet_v2,
+    mobilenet_v2,
 #     efficientnet
 )
-model_type = 'inception_v3'
+model_type = 'mobilenet_v2'
 saved_model_dir = f'{model_type}'
 
 models = {
 #     'xception':xception.Xception(weights='imagenet'),
 #     'vgg16':vgg16.VGG16(weights='imagenet'),
 #     'vgg19':vgg19.VGG19(weights='imagenet'),
-    'resnet50':resnet50.ResNet50(weights='imagenet'),
+#     'resnet50':resnet50.ResNet50(weights='imagenet'),
 #     'resnet101':resnet.ResNet101(weights='imagenet'),
 #     'resnet152':resnet.ResNet152(weights='imagenet'),
 #     'resnet50_v2':resnet_v2.ResNet50V2(weights='imagenet'),
 #     'resnet101_v2':resnet_v2.ResNet101V2(weights='imagenet'),
 #     'resnet152_v2':resnet_v2.ResNet152V2(weights='imagenet'),
-    'inception_v3':inception_v3.InceptionV3(weights='imagenet'),
+#     'inception_v3':inception_v3.InceptionV3(weights='imagenet'),
 #     'inception_resnet_v2':inception_resnet_v2.InceptionResNetV2(weights='imagenet'),
 #     'mobilenet':mobilenet.MobileNet(weights='imagenet'),
 #     'densenet121':densenet.DenseNet121(weights='imagenet'),
@@ -35,7 +35,7 @@ models = {
 #     'densenet201':densenet.DenseNet201(weights='imagenet'),
 #     'nasnetlarge':nasnet.NASNetLarge(weights='imagenet'),
 #     'nasnetmobile':nasnet.NASNetMobile(weights='imagenet'),
-#     'mobilenet_v2':mobilenet_v2.MobileNetV2(weights='imagenet'),
+    'mobilenet_v2':mobilenet_v2.MobileNetV2(weights='imagenet'),
 #     'efficientnet_b0': efficientnet.EfficientNetB0(weights='imagenet'),
 #     'efficientnet_b1': efficientnet.EfficientNetB1(weights='imagenet'),
 #     'efficientnet_b2': efficientnet.EfficientNetB2(weights='imagenet'),
